f0ea52f04f663e2f553f58382770fbf66184257f
Lisa Miner <midnightmoet@gmail.com>
2023-07-20 16:49:31 -0400

    Saving point, still working on this

---
+++ script.js
@@ -0,0 +1,4 @@
+const btnGenEl = document.getElementById('btnGen');
+const btnCopyEl = document.getElementById('btnCopy');
+const btnResetEl = document.getElementById('btnReset');
+const passwordEl = document.getElementById('password');
